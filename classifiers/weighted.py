bd693ad54caae853a07aebd28d92cf5356ef4589
Edward Wawrzynek <32946898+darksteelcode@users.noreply.github.com>
2017-12-16 14:25:37 -0700

    Integrated cleaner main

--- classifiers/weighted.py
+++ classifiers/weighted.py
@@ -4,14 +4,11 @@ Training is the same as the SimpleClassifier
 Classification is done by comparing the unknown to each author's mean, in a similair way to the SimpleClassifier
 The unknown and author's mean are subtracted, but each element is multiplied by a weight to give each different feature equal importance
 '''
-#numGroups is the number of sets of data, dataSize is length of data
-#train - data is numpy array [ [[group1-data1], [group1-data2], [group1-data3]], [[group2-data1], [group2-data2], [group2-data3]], etc]
-#run - data is numpy array [data]
 class WeightedClassifier():
-    def __init__(self, numGroups, dataSize, debug=True):
-        self.numGroups = numGroups
+    def __init__(self, numAuthors, dataSize, debug=True):
         self.dataSize = dataSize
-        self.means = np.empty(shape=[numGroups, dataSize])
+        self.numAuthors = numAuthors
+        self.means = np.empty(shape=[numAuthors, dataSize])
         self.debug = debug
         #Lengths of each feature 30 for commonWords, 9 for punctuation, 1 for sentLength, 1 for diversity
         self.featureGroups = [30, 9, 1, 1] #NEEDS TO BE UPDATED FOR NEW FEATURES
@@ -26,15 +23,14 @@ class WeightedClassifier():
                 weightIndex+=1
             featureIndex+=1
 
-    def train(self, data):
+    def train(self, data, group):
         if self.debug:
             print "WeightedClassifier Training Started"
-        #for each group of data
-        for g in range(self.numGroups):
-            #Average that data into one array
-            self.means[g] = np.multiply(np.mean(data[g], axis=0), self.weights)
-        if self.debug:
-            print "WeightedClassifier Trained Successfully"
+            self.means[group] = np.multiply(np.mean(data, axis=0), self.weights)
+        return self.means
+
+    def setGroupAvg(self, avg, group):
+        self.means[group] = np.multiply(avg, self.weights)
         return self.means
 
     def dist(self, p1, p2):
@@ -48,7 +44,7 @@ class WeightedClassifier():
         #set minDist to infinity
         minDist = np.inf
         minGroupIndex = 0
-        for g in range(self.numGroups):
+        for g in range(self.numAuthors):
             d = self.dist(data, self.means[g])
             if d < minDist:
                 minDist = d
--- main.py
+++ main.py
@@ -1,121 +1,43 @@
 import numpy as np
 import time
 import glob
-import calcFeatures
 import calcAuthor
+import AUTHORS
 from classifiers import weighted
 debug = True
 authors = ["Jane Austen", "Walter Scott", "Arthur Conan Doyle", "Charles Dickens", "Mark Twain", "Louisa Alcott"]
 #Index coresponds to authors array
 textDirs = ["texts/Jane_Austen", "texts/Walter_Scott", "texts/Arthur_Doyle", "texts/Charles_Dickens", "texts/Mark_Twain", "texts/Louisa_Alcott"]
-#Number of samples per author
-numSamples = 5
-#Data for classifier training
-calculatedData = [[[]]*numSamples for i in range(len(authors))]
-featureCalcs = []
-#Create a FeatureCalculator for each text
-for d in textDirs:
-    featureCalcs.append([])
-    for f in glob.glob(d + "/*.txt"):
-        textFile = open(f, 'r')
-        featureCalcs[len(featureCalcs)-1].append(calcFeatures.FeatureCalculator(textFile.read(),f,debug=debug))
-        textFile.close()
-
 unknownDir = "texts/Unknown"
-unknownAttributions = []
-#Used only to measure accuracy, not required(set to False if authors not known)
-realAttributionsFile = "texts/Unknown/AUTHORS.txt"
-realAttributions = []
-startTime = time.time()
+#List of guessed attributions in format [[name, authorNum], [name, authorNum]]
+attributions = []
 
-def calcUnknownFeats(classifier):
-    #Run on each work
-    i = 0
-    for f in glob.glob(unknownDir + "/*.txt"):
-        if f in realAttributionsFile:
-            continue
-        unF = open(f, 'r')
-        unCalc = calcFeatures.FeatureCalculator(unF.read(), f)
-        unF.close()
-        unFeats = unCalc.calcFeatures()
-        #Flatten to classify using one dimensional classifier
-        unFeats = [item for items in unFeats for item in items]
-        guess = classifier.run(unFeats)
-        if debug:
-            print "Guessed Author for " + f + " is Number " + str(guess) + ", " + authors[guess]
-        unknownAttributions.append([f, authors[guess]])
-        i+=1
+classifier = weighted.WeightedClassifier(len(textDirs), sum(calcAuthor.LENGTHS))
 
-def listTexts():
-    if debug:
-        print "--TEXTS--"
-        for d in textDirs:
-            print d
-        print ""
+groupIndex = 0
+for author in textDirs:
+    features = []
+    for f in glob.glob(author + "/*.txt"):
+        calculator = calcAuthor.CalcAuthorBatch(f, False, 1, True)
+        features.append(calculator.calcFeatures()[0])
+    classifier.train(features, groupIndex)
+    groupIndex+=1
 
-def start():
-    if debug:
-        print "Started"
-        print ""
+for f in glob.glob(unknownDir + "/*.txt"):
+    if "AUTHORS" in f:
+        continue
+    calculator = calcAuthor.CalcAuthorBatch(f, False, 1, True)
+    attributions.append([f, classifier.run(calculator.calcFeatures()[0])])
 
-def finish():
-    if debug:
-        print "Finished in " + str(time.time()-startTime) + " Seconds"
+correct = 0
+numFiles = 0
 
-def startFeatureCalc():
-    if debug:
-        print "--FEATURE CALCULATIONS--"
+for a in attributions:
+    print a[0] + " attributed to " + authors[a[1]]
+    real = AUTHORS.getAuthor(a[0], AUTHORS.getAttributions("texts/Unknown", authors))
+    print "Really: " + authors[real]
+    if real == a[1]:
+        correct += 1
+    numFiles+=1
 
-def readAttributionsFile():
-    if realAttributionsFile:
-        f = open(realAttributionsFile, 'r')
-        for l in f:
-            work = l.split(":")[0]
-            author = l.split(":")[1].replace("\n", "")
-            realAttributions.append([work, author])
-    if debug:
-        print "Recorded known attributions from " + realAttributionsFile
-
-def findRealAttributed(work):
-    for w in realAttributions:
-        if w[0] in work:
-            return w[1]
-    return "NOT_ATTRIBUTED"
-
-def run():
-    start()
-    listTexts()
-    startFeatureCalc()
-    authorIndex = 0
-    #Used only for setting up classifer out of loop - used ot get length of flattened features
-    featData = []
-    for author in featureCalcs:
-        textIndex = 0
-        for text in author:
-            featData = text.calcFeatures()
-            #Flatten to classify using one dimensional classifier
-            featData = [item for items in featData for item in items]
-            calculatedData[authorIndex][textIndex] = featData
-            textIndex+=1
-        authorIndex+=1
-    classifier = weighted.WeightedClassifier(len(authors), len(featData), debug)
-    classifier.train(calculatedData)
-    calcUnknownFeats(classifier)
-    readAttributionsFile()
-    print "--- Results ---"
-    correct = 0
-    for f in unknownAttributions:
-        print "------------------------------------"
-        print f[0] + " attributed to " + f[1]
-        print "Really: " + findRealAttributed(f[0])
-        if findRealAttributed(f[0]) == f[1]:
-            correct += 1
-    print
-    print str(correct) + " of " + str(len(unknownAttributions)) + " attributed correctly."
-    print
-    percent = str(round(float(correct)/float(len(unknownAttributions))*100))
-    print "*-----*\n|" + percent.ljust(5) + "|\n*-----*"
-    print  "% Correct"
-    print
-    finish()
-run()
+print round((float(correct)/float(numFiles))*100)
